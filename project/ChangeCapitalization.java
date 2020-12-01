af2e6167ac731b76dbed4ce04df9480fb161d740
Jared G <jmattgiroux@gmail.com>
2020-12-01 15:52:08 -0600

    Made a bunch of edits to the ChangeCapitalization class, but still haven't tested the environment to see if I can get it all to run, even in its current state

--- project/ChangeCapitalization.java
+++ project/ChangeCapitalization.java
@@ -9,9 +9,49 @@ import java.util.ArrayList;
 
 public class ChangeCapitalization {
     
+    //originally input string from user input or file
+    ArrayList<String> originalString;
+
+    //current string
+    ArrayList<String> currentState;
+
+    //string from previous step
+    ArrayList<String> previousStepString;
+
+    //boolean variables that control program's logic
+    //for seeing if the user has just started the program
+    boolean firstTime;
+
+    //for seeing if the user has a stringToEdit loaded into the
+    //corresponding ArrayList
+    boolean stringToEditNotLoaded;
+
+    //for seeing if user wants to quit the program
+    boolean quitProgramBool;
+
+    //ChangeCapitalization member variables;
+    String userInput;
+
+    //where the file name the user wants to read or edit is stored
+    String file;
 
 
-    ArrayList<String> arrayList = new ArrayList<String>();
+
+    //default constructor
+    ChangeCapitalization(){
+
+        //initialize member variables
+        originalString = new ArrayList<String>();
+        currentState = new ArrayList<String>();
+        previousStepString = new ArrayList<String>();
+        firstTime = true;
+        stringToEditNotLoaded = true;
+        quitProgramBool = false;
+        userInput = "";
+        file = " ";
+
+    }
+
 
     public void readFile(String file){
 
@@ -21,16 +61,166 @@ public class ChangeCapitalization {
 
     }
 
-    public void chooseEdit(){
+
+    public void upperCapitalize(){
+
+    }
+
+    public void lowerCapitalize(){
+
+    }
+
+    public void normalCapitalize(){
+
+    }
+
+    public void revertChangesOneStep(){
+
+    }
+
+    public void revertChangesToBeginning(){
+
+    }
+    
+    public void displayCurrentState(){
+
+
+    }
+
+    public void displayPrompt(){
+
+
+        String welcome = "Welcome to the Capitalization Editor!\n";
+        String initialPrompt =  "Please choose 1 option:\n" +
+                                "Load a file: F\n" +
+                                "Input a String: I\n" + 
+                                "Quit Program: Q\n";
+
+        String chooseEdit = "Please choose which edit you would like to make:\n" +
+                            "Upper Capitalization: U\n" +
+                            "Lower Capitalization: L\n" +
+                            "Normal Capitalization: N\n" +
+                            "Display Current State: D\n" +
+                            "Revert Changes Back One Step: R\n" +
+                            "Revert Current State to Original State: O\n" +
+                            "Export Current State to File: E\n" +
+                            "Quit Program: Q\n";
+
+        String quit = "Thank you. Goodbye.\n";
+
+        
+
+        if (firstTime){
+            System.out.print(welcome);
+            firstTime = false;
+        }
+
+        if (quitProgramBool)
+        {
+            System.out.println(quit);
+            return;
+        }
+            
+        if (stringToEditNotLoaded){
+            System.out.print(initialPrompt);
+            stringToEditNotLoaded = false;
+            return;
+        }
+
+        if (!stringToEditNotLoaded)
+        {
+            System.out.println(chooseEdit);
+            return;
+        }
+
+        
+
+    }
+
+    public void getUserInput(){
+
         
     }
 
-    
-    
+    public void actOnUserInput(){
 
-    
+        // String initialPrompt =  "Please choose 1 option:\n" +
+        //                         "Load a file: F\n" +
+        if (userInput == "F")
+        {
+            String file = getFileName();
+            readFile(file);
+
+        }
+        //                         "Input a String: I\n" + 
+        if (userInput == "I")
+        {
+            getStringFromUser();
+        }
+        //                         "Quit Program: Q\n";
+        if (userInput == "Q")
+        {
+            quitProgram();
+        }
+
+        // String chooseEdit = "Please choose which edit you would like to make:\n" +
+        //                     "Upper Capitalization: U\n" +
+        if (userInput == "U")
+        {
+            upperCapitalize();
+        }
+        //                     "Lower Capitalization: L\n" +
+        if (userInput == "L")
+        {
+            lowerCapitalize();
+        }
+        //                     "Normal Capitalization: N\n" +
+        if (userInput == "N")
+        {
+            normalCapitalize();
+        }
 
 
+        // "Revert Changes Back One Step: R\n" +
+        if (userInput == "R")
+        {
+            normalCapitalize();
+        }
+
+        // "Revert Current State to Original State: O \n" +
+        if (userInput == "O")
+        {
+            normalCapitalize();
+        }
+
+        //                     "Display Current State: D\n" +
+        if (userInput == "D")
+        {
+            displayCurrentState();
+        }
+        //                     "Export Current State to File: E\n" +
+        if (userInput == "E")
+        {
+            writeFile(file);
+        }
+        //                     "Quit Program: Q\n";
+        //already done above^
+    }
+
+    public String getFileName(){
+
+        String fileName = "";
+
+        return fileName;
+    }
+
+    public void getStringFromUser(){
+
+    }
+
+    public void quitProgram(){
+
+    }
 
 
 
