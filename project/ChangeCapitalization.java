b4954a451ca4a203db5432d403d1863123525613
Jared G <jmattgiroux@gmail.com>
2020-12-01 15:02:08 -0600

    First commit, began creating methods for ChangeCapitalization class; I need to figure out how to make ChangeCapitalization as a package so it can be imported into the main class.

---
+++ project/ChangeCapitalization.java
@@ -0,0 +1,37 @@
+
+//https://www.w3schools.com/java/java_packages.asp 
+
+//package java_2;
+
+//https://www.w3schools.com/java/java_arraylist.asp
+import java.util.ArrayList;
+
+
+public class ChangeCapitalization {
+    
+
+
+    ArrayList<String> arrayList = new ArrayList<String>();
+
+    public void readFile(String file){
+
+    }
+
+    public void writeFile(String file){
+
+    }
+
+    public void chooseEdit(){
+        
+    }
+
+    
+    
+
+    
+
+
+
+
+
+}
